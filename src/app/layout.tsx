f39c80d531261c546e45670718d27d0ee9919266
Roman Bekh <coldshein@gmail.com>
2024-10-10 11:36:24 +0300

    hotfix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,15 +4,15 @@ import { Kode_Mono } from "next/font/google";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "./components/Container";
+import StoreProvider from "./storeProvider";
 import TopBar from "./components/TopBar";
-import StoreProvider from "./StoreProvider";
 
 export const metadata: Metadata = {
   title: "Rick N Morty | Home library",
   description: "Welcome to library of Rick N Morty cartoon.",
 };
 
-const kodeMonoFont = Kode_Mono({ subsets: ["latin"], display: "swap", style: 'normal'});
+const kodeMonoFont = Kode_Mono({ subsets: ["latin"], display: 'swap'});
 
 export default function RootLayout({
   children,
