c3408ea008bc94875d3477ddff2c1755a7eea1a3
Viktoriia Sokolvak <sokolvak.victoria@gmail.com>
2024-10-14 13:21:11 +0300

    resolve conflict

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,40 @@
+import { NextAuthOptions } from "next-auth";
+
+import GitHubProvider from 'next-auth/providers/github';
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username:",
+          type: "text",
+          placeholder: "Choose your alternate universe name"
+        },
+        password: {
+          label: "Password:",
+          type: "password",
+          placeholder: "Password to the multiverse"
+        },
+      },
+      async authorize(credentials) {
+        const user = { id: "42", name: "Test User", password: "nextauth" };
+
+        if(credentials?.username === user.name && credentials?.password === user.password) {
+          return user;
+        } else {
+          return null;
+        }
+      }
+    })
+  ],
+}
+
+export default options;
