f70ddc3da300141553a61c353a16f283d1198e0a
Roman Bekh <coldshein@gmail.com>
2024-10-18 17:04:41 +0300

    fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-/* @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/next_rick-n-morty',
   assetPrefix: '/next_rick-n-morty',
@@ -9,18 +9,13 @@ const nextConfig = {
         protocol: "https",
         hostname: "rickandmortyapi.com",
         port: "",
-        pathname: "/api/character/avatar/",
+        pathname: "/api/character/avatar/**",
       },
+      
     ],
   },
-  env: {
-    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  },
+  
 };
 
 export default nextConfig;
+
