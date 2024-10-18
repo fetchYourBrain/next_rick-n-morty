5c611ae7efc12290ae55032823ea6533c1e64e49
Roman Bekh <coldshein@gmail.com>
2024-10-18 17:02:42 +0300

    fixed

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,6 @@ const nextConfig = {
       },
     ],
   },
-  // Додаємо змінні середовища
   env: {
     NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
