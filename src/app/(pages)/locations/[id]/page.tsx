3253183414db424811d00ef69fa960e0fca4a4e5
Vadym Urbanovych <vadymurbanovych@example.com>
2024-10-19 10:44:07 +0200

    fixed styles

--- src/app/(pages)/locations/[id]/page.tsx
+++ src/app/(pages)/locations/[id]/page.tsx
@@ -13,14 +13,26 @@ const LocationPage = async ({ params }: { params: { id: string } }) => {
     ? residentsResponse
     : [residentsResponse];
 
+  const { name, type, dimension } = location;
+
   const residentsCount = residents.length;
 
   return (
-    <div>
-      <h1 className="text-2xl font-bold">{location.name}</h1>
-      <h2 className="text-xl font-bold mb-8">
-        Residents: {residentsCount < 2 ? "1" : `${residentsCount} `} resident{residentsCount !== 1 && 's'}
-      </h2>
+    <div className="relative">
+    <div className="sticky top-[82px] z-[1] bg-light-card-bg dark:bg-dark-card-bg mb-10 flex flex-col gap-3 p-4 border-b-2 border-light-divider dark:border-dark-divider  font-bold ">
+      <h2 className="text-light-primary dark:text-dark-primary text-3xl">{name}</h2>
+      
+      <p>{type}</p>
+
+      <div className="flex flex-col gap-3 md:flex-row md:justify-between">
+        <p>{dimension === 'unknown' ? 'Unknown Dimension' : dimension}</p>
+        <p className="text-xl font-bold mb-8">
+          Residents: {residentsCount < 2 ? "1" : `${residentsCount} `} resident{residentsCount !== 1 && 's'}
+        </p>
+      </div>
+    </div>
+
+    <div className="relative z-0">
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 gap-y-8">
         {residents.map((resident: Character) => (
           <li key={resident.id}>
@@ -28,6 +40,7 @@ const LocationPage = async ({ params }: { params: { id: string } }) => {
           </li>
         ))}
       </ul>
+      </div>
     </div>
   );
 };
